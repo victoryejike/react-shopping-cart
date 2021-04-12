462d99e0ae5f2c76209cd2c9065fc2538209d942
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-04-12 23:28:04 +0100

    added the basic structure of the project

---
+++ src/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cart from "./cart";
+
+const Products = () => {
+  return (
+    <>
+      <div></div>
+      <Cart />
+    </>
+  );
+};
+
+export default Products;

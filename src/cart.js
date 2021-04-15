e480b7822e5092994fa583f4cafd979db3d399c2
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-04-15 16:44:35 +0100

    still yet to add the total price of goods in cart. Next step

--- src/cart.js
+++ src/cart.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 const Cart = ({ totalItems, items }) => {
-  const []
   return (
     <>
       <div>
         <p>Cart Items {totalItems}</p>
         <p>Total</p>
       </div>
-      
+
       <div>
         {items.map((item, i) => (
           <li key={i}>{item.title}</li>
         ))}
       </div>
+      <div>Total Price of goods </div>
     </>
   );
 };

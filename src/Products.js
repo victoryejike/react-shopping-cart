46c8f56961e9e1b9a8f4436440b2b27d50c87eb5
Ejike Chinecherem Victory <56884436+victoryejike@users.noreply.github.com>
2021-04-16 19:23:03 +0100

    adding a count key to the product object to keep track of the number of times a product was clicked

--- src/Products.js
+++ src/Products.js
@@ -8,25 +8,29 @@ const Products = () => {
     {
       title: "Peru Hair",
       image: "https://p.globalsources.com/IMAGES/PDT/BIG/349/B1173831349.jpg",
-      price: "$25.99"
+      price: "$25.99",
+      count: 0
     },
     {
       title: "Bournvita",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw0dK6wt1-PqSmEzWF9iEnVCeprh8J1owbQg&usqp=CAU",
-      price: "$5.79"
+      price: "$5.79",
+      count: 0
     },
     {
       title: "Cheese",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREBMxCoF_uAXtDvu1DJ5iwGXVIaMzqRVb6Uw&usqp=CAU",
-      price: "$2.49"
+      price: "$2.49",
+      count: 0
     },
     {
       title: "Soda",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF-L7eYOSaEUhNNLuFhFstW73LCxHqcteLfA&usqp=CAU",
-      price: "$4.59"
+      price: "$4.59",
+      count: 0
     }
   ]);
 
--- src/cart.js
+++ src/cart.js
@@ -5,12 +5,11 @@ const Cart = ({ totalItems, items }) => {
     <>
       <div>
         <p>Cart Items {totalItems}</p>
-        <p>Total</p>
       </div>
 
       <div>
         {items.map((item, i) => (
-          <li key={i}>{item.title}</li>
+          <li key={i}>{item.title} </li>
         ))}
       </div>
       <div>Total Price of goods </div>
